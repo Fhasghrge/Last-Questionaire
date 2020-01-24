9edb6bd84d60bce949b1e85c4f1e6c846d7d5cde
Fhasghrge <1969533391@qq.com>
2020-01-24 14:58:01 +0800

    init

---
+++ recos-last-questionaire/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <login></login>
+  </div>
+</template>
+
+<script>
+import login from './components/main'
+
+export default {
+  name: 'app',
+  components: {
+    login
+  }
+}
+</script>
+
+<style>
+@import url('./assets/css/css.css');
+@import url('./assets/css/reset.css');
+</style>
---
+++ recos-last-questionaire/src/components/main.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="login">
+    <div id="shuang">
+      <div id="avatar">
+        <img src="../assets/img/logo.jpg" />
+      </div>
+      <div id="name">毕业生</div>
+    </div>
+
+    <div class="entrance-bottom">
+      <div class="entrance-bottom-frame clearfix"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#login {
+  background-image: url('../assets/img/dtbg.png');
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    background-color: #F5FDEC;
+    height: 100%;
+}
+#shuang {
+  text-align: center;
+  line-height: 1rem;
+  font-size: 0.36rem;
+  margin: auto;
+  padding-top: 20px;
+}
+#name {
+  font-size: 1em;
+  font-weight: bold;
+  font-family: sans-serif;
+}
+#avatar > img {
+  height: 100px;
+  border-radius: 50%;
+}
+</style>
+
